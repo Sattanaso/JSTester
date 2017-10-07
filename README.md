012b096205b77019ca108816a4c283190feda211
Sattanaso <webdjsandpy@gmail.com>
2017-10-07 10:42:24 +0300

    carousel - left and right moves

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # mycuisine
-Single-page application
+### Chocolate and chocolate products single-page app
+
+to install node packages:
+
+## -> npm install
+
+to start app:
+
+## -> npm start
+
+to run sass watcher:
+
+## -> npm run sass
---
+++ app/client/business/dom.js
@@ -0,0 +1,55 @@
+// function getDropDownList(name, id, optionList) {
+// 	var combo = $("<select></select>").attr("id", id).attr("name", name);
+
+// 	$.each(optionList, function (i, el) {
+// 			combo.append("<option>" + el + "</option>");
+// 	});
+
+// 	return combo;
+// 	// OR
+// 	$("#SELECTOR").append(combo);
+// }
+
+$(() => {
+	let width = 920;
+	let animationSpeed = 2000;
+	let pause = 8000;
+	let currentSlide = 1;
+	let isRightOriented = true;
+
+	let $slider = $('#slider');
+	let $sliderContainer = $slider.find('.slides');
+	let $slides = $sliderContainer.find('.slide');
+
+	console.log($slides.length);
+
+	let interval;
+
+	function startSlider() {
+		interval = setInterval(() => {
+
+			if (isRightOriented) {
+				$sliderContainer.animate({ 'margin-left': '-=' + width }, animationSpeed, () => {
+					++currentSlide;
+					if (currentSlide === $slides.length) {
+						isRightOriented = !isRightOriented;
+					}
+				});
+			} else {
+				$sliderContainer.animate({ 'margin-left': '+=' + width }, animationSpeed, () => {
+					--currentSlide;
+					if (currentSlide === 1) {
+						isRightOriented = !isRightOriented;
+					}
+				});
+			}
+		}, pause);
+	}
+
+	function stopSlider() {
+		clearInterval(interval);
+	}
+
+	$slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+	startSlider();
+})
